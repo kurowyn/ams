d8353f89f1af1d83505a2e378d35d001a65e5c6c
aminove49 <aidoudimohamedaminegithub@gmail.com>
2026-01-18 11:56:12 +0100

    Delete min project v2 directory

--- min project v2/src/com/miniproject/model/Enseignant.java
+++
@@ -1,7 +0,0 @@
-package com.miniproject.model;
-
-public class Enseignant extends User {
-    public Enseignant(int id, String nom, String prenom, String login, String pwd) {
-        super(id, nom, prenom, login, pwd, "ENSEIGNANT");
-    }
-}
