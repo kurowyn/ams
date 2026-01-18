3cf5fe9cb5fb0cb3c9ba1bdfafdc0162d251ac4e
aminove49 <aidoudimohamedaminegithub@gmail.com>
2026-01-18 14:00:59 +0100

    Delete src/com/miniproject directory

--- src/com/miniproject/model/Admin.java
+++
@@ -1,7 +0,0 @@
-package com.miniproject.model;
-
-public class Admin extends User {
-    public Admin(int id, String firstName, String lastName, String userName, String password) {
-        super(id, firstName, lastName, userName, password, "ADMIN");
-    }
-}
