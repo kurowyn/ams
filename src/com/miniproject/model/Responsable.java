5d46d74e9011cf234d6362887d46a873c12b2658
aminove49 <aidoudimohamedaminegithub@gmail.com>
2026-01-18 13:27:04 +0100

    Delete src/com/miniproject directory

--- src/com/miniproject/model/Responsable.java
+++
@@ -1,7 +0,0 @@
-package com.miniproject.model;
-
-public class Responsable extends User {
-    public Responsable(int id, String nom, String prenom, String login, String pwd) {
-        super(id, nom, prenom, login, pwd, "RESPONSABLE");
-    }
-}
